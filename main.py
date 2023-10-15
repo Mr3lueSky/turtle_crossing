27c818ca09e6120b2ea3c9521f43f97e259b95b1
Jyothish T R <trjyothish0@gmail.com>
2023-10-15 13:35:02 +0530

    test commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+print("hi")
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
